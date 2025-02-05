cc79cab124bba831596f11d299db0e92be356ba0
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-02-05 20:25:02 -0300

    :ambulance: corrigindo os testes ainda não realizados

--- src/__test__/005.test.ts
+++ src/__test__/005.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, test } from "@jest/globals";
 
 describe("Validação de array de objetos", () => {
   test("WIP", () => {
-    expect(1 + 1).toBe(3); // Isso sempre será falso
+    expect(1 + 1).toBe(2); // Isso sempre será falso
   });
 });
