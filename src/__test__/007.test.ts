ab63016a1b13b81e0a62fc0775b96f714eb52a3b
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-05-14 22:45:23 -0300

    :construction: WIP

--- src/__test__/007.test.ts
+++ src/__test__/007.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, test } from "@jest/globals";
 
 describe("Validação de Estruturas Diferentes com base em uma chave", () => {
   test("WIP", () => {
-    expect(1 + 1).toBe(2); // Isso sempre será falso
+    expect(1 + 1).toBe(3); // Isso sempre será falso
   });
 });
